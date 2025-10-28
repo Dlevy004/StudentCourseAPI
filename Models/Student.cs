f6347bef680bdb8f6eabd48bfe7f2747fc1508a7
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2025-10-28 09:12:08 +0100

    feat(backend): add Student validation

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,12 +1,20 @@
-﻿namespace StudentCourseAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentCourseAPI.Models
 {
     public class Student
     {
+        [Required]
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(30)]
         public string Name { get; set; }
 
         public int Age { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
