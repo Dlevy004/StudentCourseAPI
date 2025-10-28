bb466d492ac4f5710354ad3add9a6442e0cc347d
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2025-10-28 08:26:10 +0100

    feat(backend): add Course model

---
+++ Models/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentCourseAPI.Models
+{
+    public class Course
+    {
+        private int Id { get; set; }
+
+        private string Title { get; set; }
+
+        private int Credits { get; set; }
+    }
+}
