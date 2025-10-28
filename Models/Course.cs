9f39c4a87068d22d1848aa91fa934cd65fc7da4b
Daróczi Levente Tibor <d.l3v1.deik@mailbox.unideb.hu>
2025-10-28 09:12:45 +0100

    feat(backend): add Course validation

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,9 +1,14 @@
-﻿namespace StudentCourseAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentCourseAPI.Models
 {
     public class Course
     {
+        [Required]
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
 
         public int Credits { get; set; }
